60e42d57269310cd390e37a7a834a88dfe27e917
Ashish Shubham <ashish.shubham@thoughtspot.com>
2020-03-19 17:16:41 -0700

    Updates

---
+++ precache-manifest.ffa069b6a035c23b1416195596771b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc5068e671ca842b4f2680d58084fd4",
+    "url": "/pages/dev/sars-cov2-simulator/index.html"
+  },
+  {
+    "revision": "02a37e1633ec3800a2de",
+    "url": "/pages/dev/sars-cov2-simulator/static/css/main.3aae1b45.chunk.css"
+  },
+  {
+    "revision": "f775456391abeed5695e",
+    "url": "/pages/dev/sars-cov2-simulator/static/js/2.fab346e7.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/pages/dev/sars-cov2-simulator/static/js/2.fab346e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a37e1633ec3800a2de",
+    "url": "/pages/dev/sars-cov2-simulator/static/js/main.5e68941c.chunk.js"
+  },
+  {
+    "revision": "6300ac330a49531adf00",
+    "url": "/pages/dev/sars-cov2-simulator/static/js/runtime-main.aa5c77f4.js"
+  }
+]);
